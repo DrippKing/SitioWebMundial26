542e90839e9c2e2b194bf1c540ee12fccc6a531d
DrippKing <christiansalazar2005@gmail.com>
2025-11-25 12:47:14 -0600

    Update db_connection.php

--- php/db_connection.php
+++ php/db_connection.php
@@ -5,6 +5,12 @@ $contrasena_db = "";
 $nombre_db = "poi_database";
 $port = 3307;
 
+$host = "drippking.com";
+$usuario_db = "drippkin_Host";
+$contrasena_db = "DrippKing5545";
+$nombre_db = "drippkin_poi_database";
+$port = 3306;
+
 global $conexion;
 $conexion = new mysqli($host, $usuario_db, $contrasena_db, $nombre_db, $port);
 
